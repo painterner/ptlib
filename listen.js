f8a360c5a4b0255f3020b603a975ba589d2443f7
painterner <i5stiyuki@icloud.com>
2020-01-05 12:24:25 +0800

    tco m2m token

--- listen.js
+++ listen.js
@@ -2,17 +2,45 @@
 
 express = require('express')
 bodyparser = require('body-parser')
+path = require('path')
+fs = require('fs')
+const testData = require('./data/testData')
 
 app= express()
 app.use(bodyparser.urlencoded({'extended': true}))
 app.use(bodyparser.json());
 
-app.all('/', (req, res) => {
-    console.log(['req', req.method, req.params, req.query, req.body])
+app.use((req, res, next) => {
+    console.log(['req', req.method, req.path, req.params, req.query, req.body])
+    next()
+})
 
+app.all('/', (req, res) => {
     res.send('ok\n')
+})
+
+const m2m_rt = testData.testM2MReturn
+app.post('/auth/token',(req,res) => {
+  const body = req.body
+  if(body.client_secret){
+    return res.send(m2m_rt)
+  }
 
 })
+
+app.get('/download', (req, res) =>{
+  const fileName = path.join(__dirname, './nock.js')
+  const file = fs.readFileSync(fileName)
+  // res.attachment(fileName)
+  // res.send(file)
+  res.download(fileName)
+})
+
+app.all('*', (req, res) => {
+	console.log('other route')
+	res.send('ok\n')
+})
+
 
 const port = process.argv[2]
 app.listen(port, () => {
